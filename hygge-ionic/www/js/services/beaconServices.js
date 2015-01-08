26b6dfdb84b4ccb2bc32ead9350b4d427952f334
travisgalloway@me.com <travisgalloway@me.com>
2015-01-08 16:32:06 -0600

    beaconService - Adding Plugin Calls
    Currently caught at ‘Invalid callback id received by sendPluginResult’

--- hygge-ionic/www/js/services/beaconServices.js
+++ hygge-ionic/www/js/services/beaconServices.js
@@ -1,9 +1,9 @@
 angular.module('hygge.beaconServices', ['ionic'])
 
-.factory('beaconScan', function ($ionicPlatform) {
+.factory('beaconScan', function ($ionicPlatform, $log) {
 
-  var region = [{uuid:'113F44CF-4850-4891-B48C-0E5A337DF580'}];
-  var beacons = [];
+  var region = '113F44CF-4850-4891-B48C-0E5A337DF580';
+  var beacons = {};
 
   // Mock data for console UI debug
   if (!window.cordova) {
@@ -28,8 +28,37 @@ angular.module('hygge.beaconServices', ['ionic'])
   $ionicPlatform.ready(function() {
 
     if (window.cordova) {
-      var rangedBeacons = cordova.plugins.locationManager.getRangedRegions();
-      console.log(rangedBeacons);
+
+      window.locationManager = cordova.plugins.locationManager;
+      console.log('init locationManager');
+
+      var delegate = new locationManager.Delegate();
+      console.log('init delegate');
+
+      // Called continuously when ranging beacons.
+      delegate.didRangeBeaconsInRegion = function(pluginResult)
+      {
+        console.log('didRangeBeaconsInRegion: ' + JSON.stringify(pluginResult));
+        for (var i in pluginResult.beacons )
+        {
+          // Insert beacon into table of found beacons.
+          var beacon = pluginResult.beacons[i];
+          beacon.timeStamp = Date.now();
+          var key = beacon.uuid + ':' + beacon.major + ':' + beacon.minor;
+          beacons[key] = beacon;
+        }
+      };
+
+      locationManager.setDelegate(delegate);
+      console.log('setDelegate');
+
+      locationManager.requestAlwaysAuthorization();
+      console.log('Request iOS Auth');
+
+      locationManager.startRangingBeaconsInRegion(region)
+        .fail(console.error)
+        .done();
+
     }
 
   });
