fdb407426812776f567e0b1797a2fafeea7fd3f8
travisgalloway@me.com <travisgalloway@me.com>
2015-01-07 18:23:00 -0600

    Cordova Bluetooth Plugin

--- hygge-ionic/www/js/services/beaconServices.js
+++ hygge-ionic/www/js/services/beaconServices.js
@@ -1,1 +1,22 @@
-angular.module('hygge.beaconServices', [])
+angular.module('hygge.beaconServices', ['ionic'])
+
+.factory('beaconScan', function ($ionicPlatform) {
+
+  var region = [{uuid:'113F44CF-4850-4891-B48C-0E5A337DF580'}];
+  var beacons = {};
+
+  // $ionicPlatform.ready(function() {
+  //
+  //   var rangedBeacons = cordova.plugins.locationManager.getRangedRegions();
+  //
+  //   console.log(rangedBeacons);
+  //
+  // });
+
+  return {
+    all: function () {
+      return beacons;
+    }
+  };
+
+});
