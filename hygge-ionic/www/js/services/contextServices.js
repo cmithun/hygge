0bbb7637c673eb8c69027337e65e54e6318703e3
travisgalloway@me.com <travisgalloway@me.com>
2015-01-12 17:48:34 -0600

    context - Dummy API call

--- hygge-ionic/www/js/services/contextServices.js
+++ hygge-ionic/www/js/services/contextServices.js
@@ -2,45 +2,48 @@ angular.module('hygge.contextServices', ['ionic'])
 
 .factory('contextLocations', function ($ionicPlatform, $http, $resource) {
 
-  var locations = [];
+  // var locations = [];
+  //
+  // // Mock data for console UI debug
+  // if (window.cordova) {
+  //   locations = [{
+  //     major: 13,
+  //     minor: 1,
+  //     floor: 13,
+  //     x: 50,
+  //     y: 50,
+  //     message: 'test beacon 1'
+  //   },{
+  //     major: 13,
+  //     minor: 2,
+  //     floor: 13,
+  //     x: 100,
+  //     y: 100,
+  //     message: 'test beacon 2'
+  //   }];
+  // }
+  // else {
+  //   $http.get("http://mithun-46828.azurewebsites.net/beaconsjson/?json=1")
+  //     .success(function(data, status, headers, config){
+  //       console.log('Success', status);
+  //     })
+  //     .error(function(data, status, headers, config){
+  //       console.log('Error', status);
+  //     })
+  //     .then(function(response){
+  //       var jsonData = response.data;
+  //       console.log(jsonData);
+  //     });
+  //
+  // }
+  //
+  // // Return context data to controllers
+  // return {
+  //   all: function () {
+  //     return locations;
+  //   }
+  // };
 
-  // Mock data for console UI debug
-  if (window.cordova) {
-    locations = [{
-      major: 13,
-      minor: 1,
-      floor: 13,
-      x: 50,
-      y: 50,
-      message: 'test beacon 1'
-    },{
-      major: 13,
-      minor: 2,
-      floor: 13,
-      x: 100,
-      y: 100,
-      message: 'test beacon 2'
-    }];
-  }
-  else {
-    $http.get("http://mithun-46828.azurewebsites.net/beaconsjson/?json=1")
-      .success(function(data, status, headers, config){
-        console.log('Success', status);
-      })
-      .error(function(data, status, headers, config){
-        console.log('Error', status);
-      })
-      .then(function(response){
-        var jsonData = response.data;
-        console.log(jsonData);
-      });
+  return $resource('http://private-bbb5a-contextapi.apiary-mock.com/context');
 
-  }
-
-  // Return context data to controllers
-  return {
-    all: function () {
-      return locations;
-    }
-  };
 });
