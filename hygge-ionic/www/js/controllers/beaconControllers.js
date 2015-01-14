1efdc8fd7405d2902e691445ac86dd5d4114b052
Sean <saobrien@gmail.com>
2015-01-14 08:22:33 -0600

    Version ALPHA

--- hygge-ionic/www/js/controllers/beaconControllers.js
+++ hygge-ionic/www/js/controllers/beaconControllers.js
@@ -2,6 +2,14 @@ angular.module('hygge.beaconControllers', [])
 
 .controller('BeaconCtrl', function($scope, beaconScan, contextLocations) {
 
+    $scope.floor13class = "";
+    $scope.floor12class = "";
+    $scope.floor11class = "";
+    $scope.floor10class = "";    
+    $scope.pin13display = "";
+    $scope.pin12display = "";
+    $scope.pin11display = "";
+    $scope.pin10display = "";
 
   function pollBeacons(){
     console.log("POLLING BEACONS................");
@@ -9,16 +17,50 @@ angular.module('hygge.beaconControllers', [])
 
     console.log("found this many: " + $scope.beacons.length);
 
+    // Match Beacon and Location data and pass to View
+    // Beacon = currentbeacon
+    // Location = currentlocation
     if ($scope.beacons.length > 0){
+      // Sort beacons
+      $scope.beacons.sortorder = "accuracy";
       var loc = $scope.beacons[0];
-      $scope.currentlocation = contextLocations.get(loc.major, loc.minor);
+
+      $scope.currentlocation = contextLocations.get(loc.major, loc.minor);  
+        switch($scope.currentlocation.floor){
+                case "13":
+                    $scope.floor13class = "active-floor";
+                    $scope.pin13display = "inline";
+                    break;
+                case "12":
+                    $scope.floor12class = "active-floor";
+                    $scope.pin12display = "inline";
+                    break;
+                case "11":
+                    $scope.floor11class = "active-floor";
+                    $scope.pin11display = "inline";
+                    break;
+                case "10":
+                    $scope.floor10class = "active-floor";
+                    $scope.pin10display = "inline";
+                    break;                
+                default:
+                    $scope.floor13class = "";
+                    $scope.floor12class = "";
+                    $scope.floor11class = "";
+                    $scope.floor10class = "";
+                    $scope.pin13display = "";
+                    $scope.pin12display = "";
+                    $scope.pin11display = "";
+                    $scope.pin10display = "";
+                    break;
+        }
     }
     $scope.$apply(); // This seems to be necessary.
   }
 
   pollBeacons();
 
-  var pollInterval = 10000; // 10 seconds
+  var pollInterval = 2000; // 10 seconds
   //$scope.beacons = beaconScan.all();
 
   //Use an interval timer to poll our controller
@@ -28,5 +70,10 @@ angular.module('hygge.beaconControllers', [])
   $scope.$on('$destroy', function(){
     clearInterval (becaonPollTimer);
   });
-
+    
+ $scope.doRefresh = function() {
+    pollBeacons();
+    $scope.$broadcast('scroll.refreshComplete');
+  };
+    
 });
